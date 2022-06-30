97dbc86d2b7928c9df1eafe1aafe0f94ebcebea8
actatum88 <4946@holbertonschool.com>
2022-06-29 18:38:52 -0700

    initial commit to upload files

---
+++ 0_printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: the first parameter
+ *
+ * Return: number of printed char
+ */
+
+int _printf(const char *format, ...)
+{
+}
